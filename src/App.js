9c059ed5eabfdb15ff04fa2b175976718929dc81
brunopintos <brunopintos98@gmail.com>
2023-02-02 12:22:51 -0300

    fixed infinite loop error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import Cell from "./components/Cell";
 import Table from "./components/Table";
 function App() {
-  return (
-    <Table x={50} y={50}></Table>
-  );
+  return <Table x={50} y={50}></Table>;
 }
 
 export default App;
